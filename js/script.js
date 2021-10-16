853695e0e861ac8787a86a86a8f9ce42ab777161
Nick-Mars <nickshootnick@gmail.com>
2021-10-16 15:25:17 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,30 @@
+//Task #1
+let user = {
+    name: "John",
+    surname: "Smith"
+};
+user.name = "Pete";
+delete user.name;
+console.log(user);
+
+// //Task #2
+// const user = {
+//     name:"John"
+// };
+// // Это будет работать?
+// user.name = "Pete";
+// console.log(user);
+// //Обьект обьявленый как костанта, может быть изменен.
+// //Обьявление корстанты защещает от изменений саму переменую user, а не ее содержимое.
+
+// // Task #3
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130  
+// };
+// let sum = 0;
+// for (let key in salaries) {
+//     sum += salaries[key];
+// }
+// console.log(sum);
